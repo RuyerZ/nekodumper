f82bb05cf8e6459a4cff194aaa436bfccaf6059e
ZRJ <zhangrujia@sjtu.edu.cn>
2022-05-31 00:41:41 +0800

    Fix cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,51 @@
 mod utils;
-use std::collections::HashMap;
-use rusqlite::{Connection, OptionalExtension};
-use walkdir::WalkDir;
 use anyhow::anyhow;
+use rusqlite::{Connection, OptionalExtension};
+use std::collections::HashMap;
 use utils::decrypt;
+use walkdir::WalkDir;
 
 //To hide the keywords
-const DB_DIR:&str = concat!("./databases/novelC","iwei");
-const CPT_DIR:&str = concat!("./files/novelC","iwei/reader/booksnew/");
-const KEY_DIR:&str = concat!("./files/Y2","hlcy8/");
+const DB_DIR: &str = concat!("./databases/novelC", "iwei");
+const CPT_DIR: &str = concat!("./files/novelC", "iwei/reader/booksnew/");
+const KEY_DIR: &str = concat!("./files/Y2", "hlcy8/");
 
-fn get_book(book:u64,conn:&Connection,keys:&HashMap<u64,String>) -> anyhow::Result<String> {
-    let mut stmt = conn.prepare("SELECT division_index,division_name FROM division WHERE book_id=? ORDER BY division_index")?;
+fn get_book(book: u64, conn: &Connection, keys: &HashMap<u64, String>) -> anyhow::Result<String> {
+    let mut stmt = conn.prepare(
+        "SELECT division_index,division_name FROM division WHERE book_id=? ORDER BY division_index",
+    )?;
     let mut rows = stmt.query([book])?;
-    let mut divs = Vec::<(u64,String)>::new();
+    let mut divs = Vec::<(u64, String)>::new();
     while let Some(row) = rows.next()? {
-        divs.push((row.get(0)?,row.get(1)?));
+        divs.push((row.get(0)?, row.get(1)?));
     }
     let mut stmt = conn.prepare("SELECT chapter_id,chapter_title FROM catalog1 WHERE book_id=? AND division_index=? ORDER BY chapter_index")?;
     let mut ret = String::new();
-    for (div,div_name) in divs {
+    for (div, div_name) in divs {
         ret.push_str(&div_name);
         ret.push_str("\n\n");
-        let mut rows = stmt.query([book,div])?;
+        let mut rows = stmt.query([book, div])?;
         while let Some(row) = rows.next()? {
-            let (id,title):(String,String) = (row.get(0)?,row.get(1)?);
-            let id:u64 = id.parse()?;
+            let (id, title): (String, String) = (row.get(0)?, row.get(1)?);
+            let id: u64 = id.parse()?;
             ret.push_str(&title);
             ret.push_str("\n\n");
             let get_chapter = || -> anyhow::Result<String> {
-                let ciphertext = std::fs::read_to_string(format!("{}{}/{}.txt",CPT_DIR,&book,&id))
-                    .map_err(|e| anyhow!("Find chapter {} Error:{}",&id,e))?;
-                let key = keys.get(&id).ok_or_else(|| anyhow!("Cannot find key of chapter {}",&id))?;
-                decrypt(ciphertext, key).ok_or_else(|| anyhow!("Decrypt chapter {} fail",&id))
+                let ciphertext =
+                    std::fs::read_to_string(format!("{}{}/{}.txt", CPT_DIR, &book, &id))
+                        .map_err(|e| anyhow!("Find chapter {} Error:{}", &id, e))?;
+                let key = keys
+                    .get(&id)
+                    .ok_or_else(|| anyhow!("Cannot find key of chapter {}", &id))?;
+                decrypt(ciphertext, key).ok_or_else(|| anyhow!("Decrypt chapter {} fail", &id))
             };
             match get_chapter() {
                 Ok(content) => {
                     ret.push_str(&content);
                     ret.push_str("\n\n");
-                },
+                }
                 Err(e) => {
-                    println!("{}",e);
+                    println!("{}", e);
                 }
             }
         }
@@ -48,69 +53,81 @@ fn get_book(book:u64,conn:&Connection,keys:&HashMap<u64,String>) -> anyhow::Resu
     Ok(ret)
 }
 
-fn get_book_info(book:u64,conn:&Connection) -> anyhow::Result<(String,String)> {
-    let mut book_info:Option<String> = conn.query_row(
-        "SELECT book_info from shelf_book_info where book_id=?",
-        [book],
-        |row| {
-            row.get(0)
-        }).optional()?;
-    if book_info.is_none() {
-        book_info = conn.query_row(
-            "SELECT book_info from read_history where book_id=?",
+fn get_book_info(book: u64, conn: &Connection) -> anyhow::Result<(String, String)> {
+    let mut book_info: Option<String> = conn
+        .query_row(
+            "SELECT book_info from shelf_book_info where book_id=?",
             [book],
-            |row| {
-                row.get(0)
-            }).optional()?;
+            |row| row.get(0),
+        )
+        .optional()?;
+    if book_info.is_none() {
+        book_info = conn
+            .query_row(
+                "SELECT book_info from read_history where book_id=?",
+                [book],
+                |row| row.get(0),
+            )
+            .optional()?;
     }
-    let book_info = book_info.ok_or_else(|| anyhow!("Cannot find info about book {}",book))?;
-    let json:serde_json::Value = serde_json::from_str(&book_info)?;
-    let name = json["book_name"].as_str().ok_or_else(|| anyhow!("Parse json error"))?;
-    let author = json["author_name"].as_str().ok_or_else(|| anyhow!("Parse json error"))?;
-    Ok((name.to_string(),author.to_string()))
+    let book_info = book_info.ok_or_else(|| anyhow!("Cannot find info about book {}", book))?;
+    let json: serde_json::Value = serde_json::from_str(&book_info)?;
+    let name = json["book_name"]
+        .as_str()
+        .ok_or_else(|| anyhow!("Parse json error"))?;
+    let author = json["author_name"]
+        .as_str()
+        .ok_or_else(|| anyhow!("Parse json error"))?;
+    Ok((name.to_string(), author.to_string()))
 }
 
 fn main() -> anyhow::Result<()> {
-    let keys:HashMap<_,_> = WalkDir::new(KEY_DIR).into_iter().filter_map(|e| e.ok())
+    let keys: HashMap<_, _> = WalkDir::new(KEY_DIR)
+        .into_iter()
+        .filter_map(|e| e.ok())
         .filter_map(|e| {
             let name = e.file_name().to_str()?;
             let mut b64 = base64::decode(name).ok()?;
-            if b64.len()<9 {
+            if b64.len() < 9 {
                 return None;
             }
-            b64.resize(9,0);
-            let key:u64 = String::from_utf8(b64).ok()?.parse().ok()?;
+            b64.resize(9, 0);
+            let key: u64 = String::from_utf8(b64).ok()?.parse().ok()?;
             let value = String::from_utf8(std::fs::read(e.path()).ok()?).ok()?;
-            Some((key,value))
-        }).collect();
+            Some((key, value))
+        })
+        .collect();
 
-    let books:Vec<_> = WalkDir::new(CPT_DIR).into_iter().filter_map(|e| e.ok())
+    let books: Vec<_> = WalkDir::new(CPT_DIR)
+        .into_iter()
+        .filter_map(|e| e.ok())
         .filter_map(|e| {
             let name = e.file_name().to_str()?;
-            let id:u64 = name.parse().ok()?;
+            let id: u64 = name.parse().ok()?;
             Some(id)
-        }).collect();
+        })
+        .collect();
 
     let conn = Connection::open(DB_DIR)?;
 
     for book in books {
-        println!("Extracting book {}:",book);
-        match get_book(book,&conn,&keys) {
+        println!("Extracting book {}:", book);
+        match get_book(book, &conn, &keys) {
             Ok(content) => {
-                let filename =match get_book_info(book, &conn) {
-                    Ok((name,author)) => {
-                        format!("《{}》作者：{}.txt",name,author)
-                    },
+                let filename = match get_book_info(book, &conn) {
+                    Ok((name, author)) => {
+                        format!("《{}》作者：{}.txt", name, author)
+                    }
                     Err(_) => {
-                        format!("{}.txt",book)
+                        format!("{}.txt", book)
                     }
                 };
-                if let Err(e) = std::fs::write(filename,content) {
-                    println!("Write book {} error:{}",book,e);
+                if let Err(e) = std::fs::write(filename, content) {
+                    println!("Write book {} error:{}", book, e);
                 }
-            },
+            }
             Err(e) => {
-                println!("Get book {} error:{}",book,e);
+                println!("Get book {} error:{}", book, e);
             }
         }
     }
--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,21 @@
-use crypto::{aes::{cbc_decryptor,KeySize}, blockmodes::PkcsPadding, buffer::{RefReadBuffer, RefWriteBuffer, WriteBuffer, BufferResult, ReadBuffer}, sha2::Sha256,digest::Digest};
+use crypto::{
+    aes::{cbc_decryptor, KeySize},
+    blockmodes::PkcsPadding,
+    buffer::{BufferResult, ReadBuffer, RefReadBuffer, RefWriteBuffer, WriteBuffer},
+    digest::Digest,
+    sha2::Sha256,
+};
 
-fn aes_decrypt(ciphertext:&[u8],key:&[u8]) -> Option<String> {
-    let mut decryptor = cbc_decryptor(KeySize::KeySize256,key,&[0;16],PkcsPadding);
+fn aes_decrypt(ciphertext: &[u8], key: &[u8]) -> Option<String> {
+    let mut decryptor = cbc_decryptor(KeySize::KeySize256, key, &[0; 16], PkcsPadding);
     let mut read_buffer = RefReadBuffer::new(ciphertext);
     let mut buffer = [0; 4096];
     let mut write_buffer = RefWriteBuffer::new(&mut buffer);
     let mut plain = Vec::<u8>::new();
     loop {
-        let flag = decryptor.decrypt(&mut read_buffer, &mut write_buffer, true).ok()?;
+        let flag = decryptor
+            .decrypt(&mut read_buffer, &mut write_buffer, true)
+            .ok()?;
         plain.extend_from_slice(write_buffer.take_read_buffer().take_remaining());
         if let BufferResult::BufferUnderflow = flag {
             break;
@@ -16,16 +24,19 @@ fn aes_decrypt(ciphertext:&[u8],key:&[u8]) -> Option<String> {
     String::from_utf8(plain).ok()
 }
 
-fn sha256(key:&str) -> Vec<u8> {
+fn sha256(key: &str) -> Vec<u8> {
     let mut engine = Sha256::new();
     engine.input_str(key);
-    let mut ret = [0;32];
+    let mut ret = [0; 32];
     engine.result(&mut ret);
     ret.to_vec()
 }
 
-pub fn decrypt(mut content:String,key:&str) -> Option<String> {
-    content = content.chars().filter(|c| !c.is_ascii_whitespace()).collect();
+pub fn decrypt(mut content: String, key: &str) -> Option<String> {
+    content = content
+        .chars()
+        .filter(|c| !c.is_ascii_whitespace())
+        .collect();
     let ciphertext = base64::decode(&content).unwrap();
     let key = sha256(key);
     aes_decrypt(&ciphertext, &key)
