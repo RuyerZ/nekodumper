9dcb67e4d3538671eb4736b54832f6061b1efb06
ZRJ <zhangrujia@sjtu.edu.cn>
2022-06-23 08:54:43 +0800

    Switch crate

--- src/utils.rs
+++ src/utils.rs
@@ -1,20 +1,31 @@
-use openssl::{
-    sha::sha256,
-    symm::{decrypt, Cipher},
-};
+use aes::cipher::{block_padding::Pkcs7, BlockDecryptMut, KeyIvInit};
+use sha2::{Digest, Sha256};
+type Aes256Cbc = cbc::Decryptor<aes::Aes256>;
 
-fn aes_decrypt(ciphertext: &[u8], key: &[u8]) -> Option<String> {
-    let t = Cipher::aes_256_cbc();
-    let v = decrypt(t, key, None, ciphertext).ok()?;
-    String::from_utf8(v).ok()
+fn aes_decrypt(mut ciphertext: Vec<u8>, key: &[u8]) -> Option<String> {
+    let len = Aes256Cbc::new_from_slices(key, &[0; 16])
+        .unwrap() // Unwrap because sha256's length is fixed
+        .decrypt_padded_mut::<Pkcs7>(&mut ciphertext)
+        .ok()?
+        .len();
+    ciphertext.truncate(len);
+    String::from_utf8(ciphertext).ok()
 }
 
-pub fn dec(mut content: String, key: &str) -> Option<String> {
-    content = content
-        .chars()
-        .filter(|c| !c.is_ascii_whitespace())
+fn sha256(input: &str) -> [u8; 32] {
+    let mut hasher = Sha256::new();
+    hasher.update(input);
+    hasher.finalize().into()
+}
+
+pub fn dec<T: AsRef<[u8]>>(content: T, key: &str) -> Option<String> {
+    let content: Vec<_> = content
+        .as_ref()
+        .iter()
+        .filter(|x| !x.is_ascii_whitespace())
+        .copied()
         .collect();
     let ciphertext = base64::decode(&content).ok()?;
-    let key = sha256(key.as_bytes());
-    aes_decrypt(&ciphertext, &key)
+    let key = sha256(key);
+    aes_decrypt(ciphertext, &key)
 }
